b1387fdd08f06c700e406a6242350b784f07a3b5
AlesOdraska <151876339+AlesOdraska@users.noreply.github.com>
2023-12-08 12:15:41 +0100

    Create HODINA.cs

---
+++ HODINA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HODINA
+{
+    class Program
+    {
+        public static double objem(double radius, double vyska)
+        {
+            
+            return (((radius * radius) * vyska * Math.PI)/ 3);
+        }
+
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("napis R");
+            double.TryParse(Console.ReadLine(), out double a);
+            Console.WriteLine("napis V");
+            double.TryParse(Console.ReadLine(), out double b);
+            Program.objem(a, b);
+            Console.WriteLine(Program.objem(a, b));
+            Console.ReadLine() ;
+            
+
+        }
+    }
+}
